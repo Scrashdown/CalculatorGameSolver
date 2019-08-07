e4bd943e3ff1b24db3ec2043822d1e584ec01a79
Loïc Droz <lk.droz@gmail.com>
2019-08-07 23:14:45 +0200

    Update to level 137.

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from level import *
 
-init_screen = ScreenNumber(10)
+init_screen = ScreenNumber(0)
 buttons = [
-    AddSubButton(2),
+    AddSubButton(-2),
+    AddSubButton(5),
+    MulButton(2),
     IncrementButtonsButton(1)
 ]
-goal = ScreenNumber(15)
-max_moves = 3
+goal = ScreenNumber(42)
+max_moves = 5
 
 level = Level(init_screen, buttons, goal, max_moves)
 print(list(level.solve(solve_all = True)))
